555561091759c06ff097d79e4a8741b20c876081
apa yu <rx836@hotmail.com>
2021-03-29 00:27:14 +0800

    Add require lib file

--- config/application.rb
+++ config/application.rb
@@ -42,6 +42,6 @@ module FantasyBaseball
     # in config/environments, which are processed later.
     #
     # config.time_zone = "Central Time (US & Canada)"
-    # config.eager_load_paths << Rails.root.join("extras")
+    config.eager_load_paths << Rails.root.join('lib')
   end
 end
