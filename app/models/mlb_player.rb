0e387dcd714e738c29cb3fb9246458c52d36149b
apa yu <rx836@hotmail.com>
2021-04-02 23:59:33 +0800

    Fix db migrate issue

--- app/models/mlb_player.rb
+++ app/models/mlb_player.rb
@@ -5,13 +5,13 @@
 # Table name: mlb_players
 #
 #  id                      :bigint           not null, primary key
-#  bats                    :string           default(''), not null
-#  name_display_first_last :string           default(''), not null
-#  primary_stat_type       :string           default(''), not null
-#  status                  :string           default(''), not null
-#  throws                  :string           default(''), not null
+#  bats                    :string           default(""), not null
+#  name_display_first_last :string           default(""), not null
+#  primary_stat_type       :string           default(""), not null
+#  status                  :string           default(""), not null
+#  throws                  :string           default(""), not null
 #  created_at              :datetime         not null
 #  updated_at              :datetime         not null
-#  mlb_data_player_id      :string           default(''), not null
+#  mlb_data_player_id      :string           default(""), not null
 #
 class MlbPlayer < ApplicationRecord; end
