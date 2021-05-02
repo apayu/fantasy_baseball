3ecb14100e57cc2bb028e545b7a811a215811392
apa yu <rx836@hotmail.com>
2021-05-02 23:12:24 +0800

    Add use game_log count stats

--- app/models/cpbl_player.rb
+++ app/models/cpbl_player.rb
@@ -32,11 +32,13 @@ class CpblPlayer < ApplicationRecord
   has_many :cpbl_pitching_game_logs, dependent: :restrict_with_error
   belongs_to :primary_position, class_name: 'BaseballPosition'
 
-  delegate :g, :avg, :obp, :slg, :ops, :ab, :r, :h, :d, :t, :hr,
-           :rbi, :bb, :so, :sb, to: :baseball_hitting_stat, prefix: :batting_stat, allow_nil: true
+  # delegate :g, :avg, :obp, :slg, :ops, :ab, :r, :h, :d, :t, :hr,
+  #          :rbi, :bb, :so, :sb, to: :baseball_hitting_stat, prefix: :batting_stat, allow_nil: true
+  #
+  # delegate :g, :gs, :w, :l, :sv, :bs, :hld, :cg, :sho, :ip, :h,
+  #          :r, :er, :hr, :bb, :so, :era, :whip, to: :baseball_pitching_stat, prefix: :pitching_stat, allow_nil: true
 
-  delegate :g, :gs, :w, :l, :sv, :bs, :hld, :cg, :sho, :ip, :h,
-           :r, :er, :hr, :bb, :so, :era, :whip, to: :baseball_pitching_stat, prefix: :pitching_stat, allow_nil: true
+  ABBR_NAME = { E02: '中信', EE2: '中信', L01: '統一', L02: '統一', AJL011: '樂天', AJL022: '樂天', B04: '富邦', BA3: '富邦', D01: '味全', AAA022: '味全' }.freeze
 
   def pitcher?
     primary_stat_type == 'pitching'
@@ -48,5 +50,34 @@ class CpblPlayer < ApplicationRecord
 
   def batter_stats_is_empty?
     baseball_hitting_stat.nil?
+  end
+
+
+  def attr_team_name
+    ABBR_NAME[tricode.to_sym]
+  end
+
+  def avg
+    (h.to_f / ab).round(3)
+  end
+
+  def obp
+    (h + to_base).fdiv(ab + to_base + sf).round(3)
+  end
+
+  def slg
+    (tb.to_f / ab).round(3)
+  end
+
+  def ops
+    (obp + slg).round(3)
+  end
+
+  def tb
+    (h - d - t - hr) + (d * 2) + (t * 3) + (hr * 4)
+  end
+
+  def to_base
+    bb + hbp
   end
 end
